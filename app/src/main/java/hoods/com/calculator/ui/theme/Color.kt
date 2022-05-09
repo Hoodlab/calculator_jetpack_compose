662a22afb90bd802e3f866abe632e3c4261ce1dc
hood <hoods.tech@gmail.com>
2022-05-09 10:17:39 +0300

    Initial Project Setup

---
+++ app/src/main/java/hoods/com/calculator/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package hoods.com.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val DeepBlue = Color(0xFF243C58)
+val DeepBlue500 = Color(0xFF111D2B)
+val DeepBlue200 = Color(0xFF4571A5)
+val Gold = Color(0xFFFFA41F)
